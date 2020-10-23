a2e01e8fa02c5b1d2606cb3c766e6eab0a50e547
assem22 <assem.mukhamadi@mail.ru>
2020-10-23 12:55:42 +0600

    start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Parent1Component } from './parent1/parent1.component';
+import { Parent2Component } from './parent2/parent2.component';
+import { Child1Component } from './parent1/child1/child1.component';
+import { Child2Component } from './parent1/child2/child2.component';
+import { Child3Component } from './parent2/child3/child3.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Parent1Component,
+    Parent2Component,
+    Child1Component,
+    Child2Component,
+    Child3Component
   ],
   imports: [
     BrowserModule,
